2b71bfa568857bbab21e8eb4e64fde04831ca48a
Alen George <al3ngeorge@gmail.com>
2022-03-09 17:26:32 +0530

    Problem4 commit

---
+++ problem4.js
@@ -0,0 +1,19 @@
+// ==== Problem #4 ====
+// The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
+
+let inventory = require('./cars')
+
+
+const getCarYear = (inventory) => {
+    let carYear = [];
+    
+    for (let index=0;index < inventory.length; index++){
+
+        carYear.push(inventory[index].car_year);
+
+    }
+
+    return carYear
+}
+
+console.log(getCarYear(inventory))
