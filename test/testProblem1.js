f2674eaf50d1543dd634bf2dddf6b4493adc1a1c
Alen George <al3ngeorge@gmail.com>
2022-03-09 18:35:49 +0530

    Refactor Test1 files

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,3 +1,16 @@
 
+const inventory = require('../cars')
 let test1 = require('../problem1')
 
+let getDataWithId = test1.getDataWithId
+ 
+// console.log(getDataWithId(inventory,34))
+let carinfo = getDataWithId(inventory, );
+
+if (carinfo) {
+  console.log(
+    `Car ${carinfo.id} is a ${carinfo.car_year} ${carinfo.car_make} ${carinfo.car_model}`
+  );
+} else {
+  console.log("No Data Found / Argument missing");
+}
